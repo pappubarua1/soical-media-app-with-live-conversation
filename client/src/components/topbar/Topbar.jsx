eac5e4e478b7a927567f37a312be10bdb032446b
Pappu Barua <pappubarua95@gmail.com>
2021-09-29 02:49:58 +0600

    updated!

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,7 +2,7 @@ import "./topbar.css"
 export default function Topbar() {
     return (
         <div className="topbarContainer">
-        .topbarLeft
+       <div className="topbarLeft"></div>
         </div>
     )
 }
