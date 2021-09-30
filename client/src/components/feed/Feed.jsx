edeec6bee9291bec297c48f76541dbdea2cc68bd
Pappu Barua <pappubarua95@gmail.com>
2021-09-30 11:51:22 +0600

    updated files

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -5,7 +5,7 @@ import { Posts } from "../../dummyData";
 export default function Feed() {
     return (
         <div className="feed">
-            <div className="feedwrapper">
+            <div className="feedWrapper">
                 <Share />
                 {Posts.map(p => (
                     <Post key={p.id} post={p} />
