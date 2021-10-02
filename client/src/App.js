91620b0ae69db98dbf8da46c5a24b9496ff47e1a
Pappu Barua <pappubarua95@gmail.com>
2021-10-02 19:16:00 +0600

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}
+  from "react-router-dom";
 
 function App() {
-
   return <Profile />;
 }
 
