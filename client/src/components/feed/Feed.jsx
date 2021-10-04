44b971d90a3dc45ca7cc5211f8639660ac57065b
Pappu Barua <pappubarua95@gmail.com>
2021-10-04 20:43:36 +0600

    updated files

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,8 +1,19 @@
 import Share from "../share/Share";
 import './feed.css';
 import Post from './../post/Post';
-import { Posts } from "../../dummyData";
+import { useEffect, useState } from 'react';
+import axios from "axios";
 export default function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("posts/timeline/61534929458dfd9efcd2ea3b");
+            setPosts(res.data);
+        };
+        fetchPosts();
+    }, [])
+
     return (
         <div className="feed">
             <div className="feedWrapper">
